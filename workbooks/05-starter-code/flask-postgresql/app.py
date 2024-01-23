2cc9547085b4d5c0bab4ccb3aff65bcc985256ef
rslim087a <42172016+rslim087a@users.noreply.github.com>
2024-01-22 21:46:37 -0500

    Update app.py

--- workbooks/05-starter-code/flask-postgresql/app.py
+++ workbooks/05-starter-code/flask-postgresql/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # Configure PostgreSQL connection
 db_config = {
-    'host': os.getenv('DATABASE', 'default_host')
+    'host': os.getenv('DATABASE', 'default_host'),
     'user': os.getenv('USER', 'default_user'),
     'password': os.getenv('PASSWORD', 'default_password'),
     'dbname': os.getenv('DATABASE', 'default_db')
