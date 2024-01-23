6bac45cd850080ecbdb6c8a59d4c61335a8b359a
Rayan Slim <rayanslim@Rayans-MacBook-Air.local>
2024-01-23 11:31:28 -0500

    refactor

--- workbooks/05-starter-code/flask-postgresql/app.py
+++ workbooks/05-starter-code/flask-postgresql/app.py
@@ -6,20 +6,28 @@ app = Flask(__name__)
 
 # Configure PostgreSQL connection
 db_config = {
-    'host': os.getenv('DATABASE'),
-    'user': os.getenv('DATABASE_USER'),
-    'password': os.getenv('DATABASE_PASSWORD'),
-    'dbname': os.getenv('DATABASE')
+    'host': os.getenv('POSTGRES_HOST'),
+    'user': os.getenv('POSTGRES_USER'),
+    'password': os.getenv('POSTGRES_PASSWORD'),
+    'dbname': os.getenv('POSTGRES_DB')
 }
+
+# Check for missing environment variables
+for key, value in db_config.items():
+    if value is None:
+        raise RuntimeError(f"Environment variable for '{key}' is not set.")
 
 @app.route('/', methods=['GET'])
 def index():
+    # Prepare a message with the database connection details
+    connection_details = f"Database Host: {db_config['host']}, User: {db_config['user']}, Password: {db_config['password']}, Database: {db_config['dbname']}"
+
     try:
         conn = psycopg2.connect(**db_config)
         conn.close()
-        return "This app is successfully connected to PostgreSQL."
+        return f"Success! Connected to an instance of PostgreSQL. You are using the credentials: {connection_details}"
     except psycopg2.Error as e:
-        return "Failed to connect this application to PostgreSQL."
+        return f"Failed to connect to an instance of PostgreSQL. You are using the credentials: {connection_details}"
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=3000)
