cd6c3715c706861ba217298489d68047a06de119
Rayan Slim <rayanslim@Rayans-MacBook-Air.local>
2024-01-23 11:09:15 -0500

    update

--- lessons/05-starter-code/flask-mysql/app.py
+++ lessons/05-starter-code/flask-mysql/app.py
@@ -6,21 +6,24 @@ app = Flask(__name__)
 
 # Configure MySQL connection
 db_config = {
-    'host': os.getenv('DATABASE'),
-    'user': os.getenv('USER'),
-    'password': os.getenv('PASSWORD'),
-    'database': os.getenv('DATABASE')
+    'host': os.getenv('DATABASE_HOST'),
+    'user': os.getenv('DATABASE_USER'),
+    'password': os.getenv('DATABASE_PASSWORD'),
+    'database': os.getenv('DATABASE_NAME')
 }
 
 
 @app.route('/', methods=['GET'])
 def index():
+    # Prepare a message with the database connection details
+    connection_details = f"Database Host: {db_config['host']}, User: {db_config['user']}, Database: {db_config['database']}"
+
     try:
         conn = mysql.connector.connect(**db_config)
         conn.close()
-        return "This app is successfully connected to MySQL."
+        return f"Success! Connected to MySQL. {connection_details}"
     except mysql.connector.Error as e:
-        return "You need to connect this application to MYSQL"
+        return f"Failed to connect to MySQL. {connection_details} Error: {e}"
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=3000)
